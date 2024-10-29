e33ff28d3c36faf58da3aa30d1d2e49c5ec14c36
Lionel Li <lizicheng86@gmail.com>
2024-10-29 09:22:59 -0400

    draft

--- src/main/scala/chapter4/exercise/EvalClient.scala
+++ src/main/scala/chapter4/exercise/EvalClient.scala
@@ -5,15 +5,15 @@ import cats.Eval
 object EvalClient extends App {
 
   def foldRight[A, B](as: List[A], acc: B)(fn: (A, B) => B): B =
-    foldRightEval(as, Eval.now(acc)) { (a, b) =>
-      b.map(fn(a, _))
-    }.value
-//    as match {
-//      case head :: tail =>
-//        fn(head, foldRight(tail, acc)(fn))
-//      case Nil =>
-//        acc
-//    }
+//    foldRightEval(as, Eval.now(acc)) { (a, b) =>
+//      b.map(fn(a, _))
+//    }.value
+    as match {
+      case head :: tail =>
+        fn(head, foldRight(tail, acc)(fn))
+      case Nil =>
+        acc
+    }
 
   def foldRightEval[A, B](as: List[A], acc: Eval[B])
                          (fn: (A, Eval[B]) => Eval[B]): Eval[B] =
